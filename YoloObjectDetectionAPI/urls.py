1c78522389531efc1286111ac98cb7555a365dab
polina_storozh <58592174+voird@users.noreply.github.com>
2024-12-02 08:55:34 +0300

    Update urls.py

--- YoloObjectDetectionAPI/urls.py
+++ YoloObjectDetectionAPI/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UploadImageView, process_image, index, upload_image
+from .views import UploadImageView, process_image, index, upload_image, upload_i
 
 urlpatterns = [
     path('', index, name='index'),
     path('process_image/', upload_image, name='process_image'),
-    path('upload/', UploadImageView.as_view(), name='upload-image'),
+    path('upload/', UploadImageView.as_view(), name='upload_image'),
+    path('upload_image/', upload_i, name='upload_image_i'),
 ]
